c7366cdc4ee8fbfb070743391a1a1cd6b1864064
IrinaAfinogenova <afinogenovair1996@gmail.com>
2025-08-23 10:27:31 +0200

    update calendar stypes

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,11 +3,13 @@ import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import { useStore } from "../store";
 import './calendar.css'; // TODO Should be fixed by Tailwind version update
+import { filterExpenseTransactions, filterIncomeTransactions } from "../utils/transactions";
 
 export function Calendar() {
   const navigate = useNavigate();
   const { transactions } = useStore(); // TODO this should not be in this component
-  const highlightedDays = transactions.map((transaction) => new Date(transaction.date));
+  const highlightedDaysRed = filterExpenseTransactions(transactions).map((transaction) => new Date(transaction.date));
+  const highlightedDaysGreen = filterIncomeTransactions(transactions).map((transaction) => new Date(transaction.date));
 
   const handleSelect = (date: Date | undefined) => {
     if (date) {
@@ -21,11 +23,16 @@ export function Calendar() {
         animate
         navLayout="around"
         mode="single"
+        classNames={{
+          today: `font-bold`,
+      }}
         modifiers={{
-          highlighted: highlightedDays,
+          highlightedRed: highlightedDaysRed,
+          highlightedGreen: highlightedDaysGreen
         }}
         modifiersClassNames={{
-          highlighted: "dot-top-right", // TODO make different colors for different types of transactions
+          highlightedRed: "dot-top-right", 
+          highlightedGreen: "dot-green",
         }}
         onSelect={handleSelect}
       />
